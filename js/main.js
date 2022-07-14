f90692c9c1016e1214ca9a544bd7f7999dfa3442
David Ward <david.a.ward@duke.edu>
2022-07-14 10:45:39 -0400

    Added both increase and decrease font functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-function DecreaseFont(){
+function IncreaseFont(){
   // this did not work: var curFontSize = document.getElementById("b_body").style.fontSize;
   var curFontSize = parseInt($('#b_body').css('font-size'));
   curFontSize = curFontSize + 2;
+  $('#b_body').css('font-size', curFontSize);
+}
+
+function DecreaseFont(){
+  // this did not work: var curFontSize = document.getElementById("b_body").style.fontSize;
+  var curFontSize = parseInt($('#b_body').css('font-size'));
+  curFontSize = curFontSize - 2;
   $('#b_body').css('font-size', curFontSize);
 }
